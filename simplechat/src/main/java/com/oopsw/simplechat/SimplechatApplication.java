a21bb4de5b4f4b528b6f92a5aa75dec7fe5ecf3c
thanhhh1005-blip <thanhhh1005@gmail.com>
2025-12-06 22:35:29 +0700

    XO-4: helloworld 4

--- simplechat/src/main/java/com/oopsw/simplechat/SimplechatApplication.java
+++ simplechat/src/main/java/com/oopsw/simplechat/SimplechatApplication.java
@@ -8,7 +8,7 @@ public class SimplechatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimplechatApplication.class, args);
-		System.out.println("hello world 3");
+		System.out.println("hello world 4");
 	}
 
 }
